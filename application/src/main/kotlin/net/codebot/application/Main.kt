c4f58616441653492794f0c2c682d858e4e2bdca
unknown <sw4wang@uwaterloo.ca>
2023-02-09 19:11:05 -0500

    Baseline Borderpane

--- application/src/main/kotlin/net/codebot/application/Main.kt
+++ application/src/main/kotlin/net/codebot/application/Main.kt
@@ -2,19 +2,55 @@ package net.codebot.application
 
 import javafx.application.Application
 import javafx.scene.Scene
+import javafx.scene.control.Button
 import javafx.scene.control.Label
+import javafx.scene.control.ScrollPane
+import javafx.scene.control.ToolBar
+import javafx.scene.layout.BorderPane
+import javafx.scene.layout.HBox
 import javafx.scene.layout.StackPane
+import javafx.scene.layout.VBox
+import javafx.scene.text.Font
+import javafx.scene.text.Text
 import javafx.stage.Stage
 import net.codebot.shared.SysInfo
 
 class Main : Application() {
     override fun start(stage: Stage) {
-        stage.scene = Scene(
-            StackPane(Label("Hello ${SysInfo.userName}")),
-            250.0,
-            150.0)
+        val toolbar =ToolBar(
+            Button("New"),
+            Button("Open"),
+            Button("Save")
+        )
+
+        val text = Text("Center")
+        text.font = Font("Helvetica", 40.0)
+        val center = VBox(text)
+
+        val label = Label("Test for Status Bar")
+        val status = HBox(label)
+
+        val left = VBox(Text("Left Pane"))
+
+        val right = VBox(Text("Right Pane"))
+
+        val border = BorderPane()
+        border.top = toolbar
+        border.center = center
+        border.bottom = status
+        border.left = left
+        border.right = right
+        /** stage.scene = Scene(
+        BorderPane(Label("Hello ${SysInfo.userName}")),
+        250.0,
+        150.0)
+         **/
+        val scene = Scene(border)
+        stage.width = 250.0
+        stage.height = 150.0
         stage.isResizable = false
         stage.title = "GUI Project"
+        stage.scene = scene
         stage.show()
     }
 }
