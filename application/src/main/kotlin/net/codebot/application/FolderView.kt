a6134e91c39dcda63f02c5196f4d54af5e3ba5d1
unknown <sw4wang@uwaterloo.ca>
2023-02-15 16:31:25 -0500

    Added File Structure for Left Pane

---
+++ application/src/main/kotlin/net/codebot/application/FolderView.kt
@@ -0,0 +1,24 @@
+package net.codebot.application
+import javafx.scene.Node
+import javafx.scene.control.TreeItem
+import javafx.scene.control.TreeView
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
+import javafx.scene.layout.StackPane
+import java.io.File
+
+class FolderView {
+    fun build() : StackPane {
+        val folder_img = ImageView(Image("https://png.pngtree.com/png-vector/20190916/ourmid/pngtree-folder-icon-for-your-project-png-image_1731079.jpg"))
+        folder_img.fitWidth = 20.0
+        folder_img.fitHeight = 20.0
+        val folderIcon: Node = folder_img
+        val rootItem = TreeItem<Any?>("Inbox", folderIcon)
+        rootItem.isExpanded = true
+        File("src/main/kotlin").walk(FileWalkDirection.BOTTOM_UP).forEach {
+            val item = TreeItem<Any?>(it)
+            rootItem.children.add(item)
+        }
+        return StackPane(TreeView<Any?>(rootItem))
+    }
+}
