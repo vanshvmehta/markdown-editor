cae17b88f666444c52f34617bc81aef01c5d91b9
unknown <sw4wang@uwaterloo.ca>
2023-02-17 17:26:26 -0500

    Added better default text and fixed file directory output + parent file

--- application/src/main/kotlin/net/codebot/application/FolderView.kt
+++ application/src/main/kotlin/net/codebot/application/FolderView.kt
@@ -8,16 +8,24 @@ import javafx.scene.layout.StackPane
 import java.io.File
 
 class FolderView {
-    fun build(pathname: String = "src/main/kotlin") : StackPane {
+    fun build(pathname: String = "No Working Directory", dir_selected: Boolean = false) : StackPane {
         val folder_img = ImageView(Image("https://png.pngtree.com/png-vector/20190916/ourmid/pngtree-folder-icon-for-your-project-png-image_1731079.jpg"))
         folder_img.fitWidth = 20.0
         folder_img.fitHeight = 20.0
         val folderIcon: Node = folder_img
-        val rootItem = TreeItem<Any?>("Inbox", folderIcon)
+        val rootItem = TreeItem<Any?>(pathname, folderIcon)
         rootItem.isExpanded = true
-        File(pathname).walk(FileWalkDirection.BOTTOM_UP).forEach {
-            val item = TreeItem<Any?>(it)
-            rootItem.children.add(item)
+        if (dir_selected) {
+            val len = pathname.length
+            File(pathname)
+                .walk(FileWalkDirection.BOTTOM_UP)
+                .maxDepth(1)
+                .sortedBy { it.isDirectory }
+                .forEach {
+                    val item = TreeItem<Any?>(it.absolutePath.substring(len))
+                    rootItem.children.add(item)
+            }
+            rootItem.children.removeLast()
         }
         return StackPane(TreeView<Any?>(rootItem))
     }
