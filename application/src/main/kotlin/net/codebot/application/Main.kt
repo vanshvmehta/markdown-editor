bf15ee02f57b402f7d6d921f7313add7aac7486e
Dan Duran <danielduranrojas@gmail.com>
2023-03-29 15:57:31 -0400

    pretties up fonts in toolbar

--- application/src/main/kotlin/net/codebot/application/Main.kt
+++ application/src/main/kotlin/net/codebot/application/Main.kt
@@ -201,10 +201,10 @@ class Main : Application() {
 
                 if (cur_theme == "darkMode.css"){
                     webView.engine.userStyleSheetLocation = "data:,body { color:#FFFFFF; background-color: #707070;" +
-                            " font:" + combo.text + "px " + compilefont + "; }"
+                            " font:" + combo.text + "px " + oldcompfont + "; }"
                     println("REACHED")
                 } else {
-                    webView.engine.userStyleSheetLocation = "data:,body { font: " + combo.text + "px " + compilefont + "; }";
+                    webView.engine.userStyleSheetLocation = "data:,body { font: " + combo.text + "px " + oldcompfont + "; }";
                     println("reached light theme")
                 }
             }
